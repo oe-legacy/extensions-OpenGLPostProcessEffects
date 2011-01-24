b6ac7204421a2638a8dcf0462e4295198c6faa1c
zerny <zerny@openengine.dk>
2011-01-24 01:01:06 +0100

    Underwater.* -> UnderWater.*, patch 1
    Ignore-this: 21a4d2db9927e0537928e941a0166322
    
    darcs-hash:20110124000106-50fb2-dd2f805b72758660c924aaddb8f8a12dc171ed2d.gz

--- shaders/Underwater.glsl
+++
@@ -1,6 +0,0 @@
-# Underwater shader program
-
-vert: extensions/OpenGLPostProcessEffects/shaders/default.glsl.vert
-frag: extensions/OpenGLPostProcessEffects/shaders/Underwater.frag
-
-unif: clearColor = 0.12 0.16 0.35 1.0
